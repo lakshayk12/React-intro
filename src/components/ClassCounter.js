57e8a2eab60745c235eebc3ef865f930289249df
Lakshay Kumar <lakshayk12@yahoo.com>
2020-12-10 01:30:38 +0530

    Update ClassCounter.js
    Can also do this.

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -10,7 +10,12 @@ class ClassCounter extends Component {
     }
 
     incrementCount = () => {
-        this.setState({ count: this.state.count + 1 });
+        // this.setState({ count: this.state.count + 1 });
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        });
     }
 
     render() {

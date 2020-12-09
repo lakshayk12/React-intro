ae4ab1654356a6d1f08ba44d3938b43efc5fd884
Lakshay Kumar <lakshayk12@yahoo.com>
2020-12-10 01:05:19 +0530

    Update HookCounter.js
    FYI

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -12,3 +12,6 @@ function HookCounter() {
 }
 
 export default HookCounter
+
+//1. Only call hooks at the top level of the functional component. Dont call inside loops, conditions & nested functions.
+//2. Only call hooks from react functions. call them from react functional components, not from regular js components.

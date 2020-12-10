1d203eceb1eece7f817e6969c32f2ab1549442f2
Lakshay Kumar <lakshayk12@yahoo.com>
2020-12-11 01:53:50 +0530

    useEffect part 2
    - conditionally updating the title of the document in class component VS in functional component using useEffect

--- src/components/ClassCounter1.js
+++ src/components/ClassCounter1.js
@@ -5,7 +5,8 @@ class ClassCounter1 extends Component {
         super(props)
 
         this.state = {
-            count: 0
+            count: 0,
+            name: ""
         }
     }
 
@@ -14,15 +15,28 @@ class ClassCounter1 extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked ${this.state.count} times`;
+        //conditionally updating the title when the appropriate variable changes.
+        //else, title was updating as soon as i start writing in input field.
+        if (prevState.count != this.state.count) {
+            console.log("In componentDidUpdate");
+            document.title = `Clicked ${this.state.count} times`;
+        }
     }
 
     render() {
         return (
             <div>
+                <input
+                    value={this.state.name}
+                    onChange={(e) => {
+                        this.setState({ name: e.target.value })
+                    }}
+                >
+                </input>
                 <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>
                     Clicked {this.state.count} times
                 </button>
+                <h1>{this.state.name}</h1>
             </div>
         )
     }
--- src/components/HookCounter1.js
+++ src/components/HookCounter1.js
@@ -1,24 +1,43 @@
 // const { Component } = require("react")
 
 // -   The Effect hook lets to perform side effects in functional Component.
-// -   It is close replacement for componentDidMount, componentDidUpdate & componentWillUnmount.
+// -   It is close replacement for 3 different fn(s) componentDidMount, componentDidUpdate & componentWillUnmount.
 // -   that Means, three life cycle methods can be handled by the useEffect.
 import React, { useEffect, useState } from 'react'
 
 function HookCounter1() {
     const [count, setCount] = useState(0);
 
+    // CONDITIONALLY run Effects:
+    // -   useEffect hook runs after every single render.
+    // -   it may cause our application to be slow.
+    // -   We need a way to conditionally run an Effect from the functional component
+    const [name, setName] = useState("");
+
     // its a function, it takes a fn as an argument & executes it after every render.
     useEffect(() => {
+        console.log("In useEffect");
         document.title = `Clicked ${count} times`;
-    })
-    //cool right?, we dont need to use three different fn(s) to achieve this unlike see ClassCounter1.js
+    }, [count])
+    //conditionally run an Effect, put a second parameter to the useEffect(), that is an array of values
+    //that specifies props or state to watch for.
+    //only if those props or state changes the Effect will be executed.
+
+    //Advantage of useEffect: cool.., we dont need to use 3 different fn(s) to achieve this unlike see ClassCounter1.js
 
     return (
         <div>
+            <input
+                value={name}
+                onChange={(e) => {
+                    setName(e.target.value)
+                }}
+            >
+            </input>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>
                 Clicked {count} times
             </button>
+            <h1>{name}</h1>
         </div>
     )
 }

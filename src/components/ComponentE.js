46a0609e0d45af89e687cb16c1330ae7607186ea
Lakshay Kumar <lakshayk12@yahoo.com>
2020-12-13 00:57:53 +0530

    useContext hook in react
    -Better alternative of context.
    -Creating a context & providing a context step remains the same.
    -useContext hook makes only the consumption of the context value simpler

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ComponentF from './ComponentF'
+import { UserContext, ChannelContext } from '../App'
 
 function ComponentE() {
+    const user = useContext(UserContext)
+    const surname = useContext(ChannelContext)
     return (
         <div>
-            <ComponentF />
+            {/* <ComponentF /> */}
+            {user} {surname}
+            {/* We can see how easy it had made context consumption :D */}
         </div>
     )
 }
